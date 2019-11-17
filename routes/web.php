d23a7cdffa12e12f885b8e7ad9fc3bb2d8f6e4a3
Doni Setiawan <donisetiawan.050@gmail.com>
2019-11-17 20:14:21 +0700

    Writing routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect('cats');
+});
+Route::get('cats', function() {
+    return 'All cats';
+});
+Route::get('cats/{id}', function ($id) {
+    return sprintf('Cat #%d', $id);
+})->where('id', '[0-9]+');
+Route::get('about', function() {
+    return view('about')->with('number_of_cats', 9000);
 });
