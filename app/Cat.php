670cd0a72d842affee50f6fdacd7c05149ac7fd1
Doni Setiawan <donisetiawan.050@gmail.com>
2019-11-18 18:07:49 +0700

    Eloquent ORM

--- app/Cat.php
+++ app/Cat.php
@@ -6,10 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model
 {
+//    use SoftDeletes;
+
+    protected $table    = 'cats';
     protected $fillable = ['name', 'date_of_birth', 'breed_id'];
+
+//    protected $dates    = ['deleted_at'];
 
     public function breed()
     {
         return $this->belongsTo(Breed::class);
+    }
+
+    public function scopeOfBreed($query, $breedId)
+    {
+        return $query->where('breed_id', '=', $breedId);
     }
 }
