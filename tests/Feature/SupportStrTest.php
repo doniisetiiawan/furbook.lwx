69dab28c9fe680974189c3f1b93fecba9638714a
Doni Setiawan <donisetiawan.050@gmail.com>
2019-11-22 17:16:16 +0700

    Unit testing with PHPUnit

---
+++ tests/Feature/SupportStrTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class SupportStrTest extends TestCase
+{
+    public function testIs()
+    {
+        $this->assertTrue(Str::is('/', '/'));
+        $this->assertFalse(Str::is('/', ' /'));
+        $this->assertFalse(Str::is('/', '/a'));
+        $this->assertTrue(Str::is('foo/*', 'foo/bar/baz'));
+        $this->assertTrue(Str::is('*/foo', 'blah/baz/foo'));
+    }
+}
